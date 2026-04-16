b90abe2cf1c45e7c35e0e7588fa73569aa556f03
Kiet Le <ktlbusiness11@gmail.com>
2026-04-16 00:46:44 -0400

    Linting and formatting

--- rtl.f
+++ rtl.f
@@ -1,4 +1,9 @@
+# 1. Submodules (i.e. -f lib/keccak-fips202-sv/rtl.f)
+
+# 2. Local Packages (i.e., rtl/my_pkg.sv)
 rtl/qrem_mem_map_pkg.sv
+
+# 3. Local RTL (i.e., rtl/transcoder_unit.sv)
 rtl/cmi.sv
 rtl/mem_addr_map.sv
 rtl/mem_arbiter.sv
